ad5b3e9656e22ff62ba2ebb9feabb393c8b29da7
Sidney <sidneyjunior26@gmail.com>
2020-08-03 08:32:11 -0300

    First commit

---
+++ FilmeAPI.dpr
@@ -0,0 +1,14 @@
+program FilmeAPI;
+
+uses
+  Vcl.Forms,
+  FormPrincipal in 'FormPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
